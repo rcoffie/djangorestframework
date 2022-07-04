d91069ea67784e356f0fd1d0ae61697ec8e1a1ba
Robert Coffie <rcoffie22@yahoo.com>
2022-07-04 09:05:02 +0000

    Django Rest_Framework ModelSerializer

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,11 +4,11 @@ endpoint = "https://httpbin.org/status/200/"
 
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello world"})
+# get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello world"})
 # print(get_response.text)
 # print(get_response.status_code)
 # print(get_response.json()['message'])
-print(get_response.json())
+# print(get_response.json())
 
 
 #
@@ -19,3 +19,5 @@ print(get_response.json())
 # printing something in a json file
 #
 # print(get_response.json())
+
+get_response = requests.post(endpoint, params={"abc":123}, json={"query":"Hello world"})
